fd851d1741c2aa8fdde41ca1e9f6d0658b15404e
Johnny Chan <chanyao@umich.edu>
2019-10-07 00:56:32 -0400

    Add identifier in partA src files

--- partA/game.h
+++ partA/game.h
@@ -1,3 +1,4 @@
+// IDENTIFIER  = 9504853406CBAC39EE89AA3AD238AA12CA198043
 #include "zombie.h"
 
 #include <queue>
--- partA/main.cpp
+++ partA/main.cpp
@@ -1,3 +1,4 @@
+// IDENTIFIER  = 9504853406CBAC39EE89AA3AD238AA12CA198043
 #include "game.h"
 
 int main(int argc, char **argv) {
